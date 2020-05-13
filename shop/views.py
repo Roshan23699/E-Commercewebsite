6a7a18b5461f4d8b82130d120a50c90f87c0f665
ma6128966 <ma6128966@gmail.com>
2020-05-13 12:07:48 +0530

    Designed Homepage

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import SlideShow
 from math import ceil
+from .models import Product
+from .models import SlideShow
+from .models import FoodGrain
+from .models import SaltSugarandJaggery
+from .models import OilsGheeandMasala
+from .models import DalandPulses
+from .models import Fruit
+from .models import Vegetable
 # Create your views here.
 
 
 def index(request):
 	# product = Product.objects.all()
 	allProds = []
-	catx = Product.objects.values('category')
+	"""catx = Product.objects.values('category')
 	cats = {item['category'] for item in catx}
 	for cat in cats:
 		product = Product.objects.filter(category=cat)
@@ -16,10 +24,10 @@ def index(request):
 		nSlides = n // 4 + ceil((n / 4) - (n // 4))
 		lenth = range(1, nSlides + 1)
 		params = [nSlides, lenth, product]
-		allProds.append([nSlides, lenth , product])
+		allProds.append([nSlides, lenth , product])"""
 
 	# allProds = [params, params, params]
-	params = {'list' : allProds}
+	params = {'list' : allProds, 'slides': SlideShow.objects.all(), 'foodgrain' : FoodGrain.objects.all(), 'saltsugjag': SaltSugarandJaggery.objects.all(), 'oilgheemasala':OilsGheeandMasala.objects.all(), 'dalpulses': DalandPulses.objects.all()}
 	return render(request, 'shop/index.html', params)
 
 def about(request):
@@ -41,4 +49,3 @@ def checkout(request):
 	return HttpResponse('we are at checkout') 
 
 
-	
