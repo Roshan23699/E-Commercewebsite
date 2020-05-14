88eab2788efb475200b8b96fede6a0f5cd6f02f5
ma6128966 <ma6128966@gmail.com>
2020-05-14 23:48:10 +0530

    Designed Grocery vegetables and fruits

--- shop/models.py
+++ shop/models.py
@@ -6,10 +6,10 @@ class Product(models.Model):
 	product_id = models.AutoField
 	product_name = models.CharField(max_length=30)
 	"""desc = models.CharField(max_length=300, default =)
-	pub_date = models.DateField(default = '')
+	pub_date = models.DateField(default = '')"""
 	category = models.CharField(max_length=300, default='')
 	subcategory = models.CharField(max_length=300, default='')
-	price = models.IntegerField(default=0)"""
+	price = models.IntegerField(default=0)
 	image = models.ImageField(upload_to='shop/images', default='')
 	def __str__(self):
 		return self.product_name
