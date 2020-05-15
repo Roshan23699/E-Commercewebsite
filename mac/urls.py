26fd7755f172139a8a095f71eb9e8ba28abd82c4
Chetas Borse <cmborse3005@gmail.com>
2020-05-15 12:32:11 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	blog/__pycache__/__init__.cpython-37.pyc
    #	blog/__pycache__/admin.cpython-37.pyc
    #	blog/__pycache__/models.cpython-37.pyc
    #	blog/migrations/__pycache__/__init__.cpython-37.pyc
    #	mac/__pycache__/__init__.cpython-37.pyc
    #	mac/__pycache__/settings.cpython-37.pyc
    #	mac/__pycache__/urls.cpython-37.pyc
    #	mac/__pycache__/wsgi.cpython-37.pyc
    #	shop/__pycache__/__init__.cpython-37.pyc
    #	shop/__pycache__/admin.cpython-37.pyc
    #	shop/__pycache__/apps.cpython-37.pyc
    #	shop/__pycache__/models.cpython-37.pyc
    #	shop/__pycache__/urls.cpython-37.pyc
    #	shop/__pycache__/views.cpython-37.pyc
    #	shop/migrations/__pycache__/0001_initial.cpython-37.pyc
    #	shop/migrations/__pycache__/0002_remove_product_category.cpython-37.pyc
    #	shop/migrations/__pycache__/__init__.cpython-37.pyc
    #	users/__pycache__/__init__.cpython-37.pyc
    #	users/__pycache__/admin.cpython-37.pyc
    #	users/__pycache__/apps.cpython-37.pyc
    #	users/__pycache__/forms.cpython-37.pyc
    #	users/__pycache__/models.cpython-37.pyc
    #	users/__pycache__/views.cpython-37.pyc
    #	users/migrations/__pycache__/__init__.cpython-37.pyc

--- mac/urls.py
+++ mac/urls.py
@@ -21,8 +21,9 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('shop/', include('shop.urls')),
+    path('', include('shop.urls')),
     path('admin/', admin.site.urls),
+    path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
